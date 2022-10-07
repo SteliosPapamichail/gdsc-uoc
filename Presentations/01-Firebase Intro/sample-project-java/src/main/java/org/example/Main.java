8fc492b2cc105aa8e29a914f73fdb6934f5cf64f
Stelios Papamichail <steliospapamichail@protonmail.com>
2022-10-07 20:59:00 +0300

    Added support for update & delete operations on firestore docs

--- Presentations/01-Firebase Intro/sample-project-java/src/main/java/org/example/Main.java
+++ Presentations/01-Firebase Intro/sample-project-java/src/main/java/org/example/Main.java
@@ -2,10 +2,7 @@ package org.example;
 
 import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.QueryDocumentSnapshot;
-import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.*;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
@@ -13,6 +10,7 @@ import com.google.firebase.cloud.FirestoreClient;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -22,7 +20,7 @@ public class Main {
         FileInputStream serviceAccount =
                 null;
         try {
-            serviceAccount = new FileInputStream("C:\\Users\\Steli\\Desktop\\GDSC-Firebase-Intro\\gdsc-firebase-intro-firebase-adminsdk-key.json");
+            serviceAccount = new FileInputStream("C:\\Users\\Steli\\Desktop\\gdsc-firebase-intro-firebase-adminsdk-key.json");
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
@@ -40,10 +38,53 @@ public class Main {
         FirebaseApp.initializeApp(options);
 
         Firestore db = FirestoreClient.getFirestore();
-        ReadAndPrintAllArtworks(db);
+        readAndPrintAllArtworks(db);
+        System.out.println("---------------------------\n");
+        createNewGalleryEntry(db, false);
+        System.out.println("---------------------------\n");
+        createNewGalleryEntry(db, true);
+        System.out.println("---------------------------\n");
+        deleteSampleArtistEntry(db);
     }
 
-    private static void ReadAndPrintAllArtworks(Firestore db) {
+    private static void deleteSampleArtistEntry(Firestore db) {
+        // asynchronously delete a document
+        ApiFuture<WriteResult> writeResult = db.collection("artists").document("pls-dont-do-this").delete();
+        try {
+            System.out.println("Entry deleted successfully!\nUpdate time : " + writeResult.get().getUpdateTime());
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void createNewGalleryEntry(Firestore db, boolean autoGenId) {
+        HashMap<String, Object> galleryInfo = new HashMap<>();
+        if (!autoGenId) {
+            galleryInfo.put("gName", "Dio Horia");
+            galleryInfo.put("location", new GeoPoint(10.051, 110.000));
+            // Add a new document (asynchronously) in collection "galleries" with id "dio_horia"
+            ApiFuture<WriteResult> future = db.collection("galleries").document("dio_horia").set(galleryInfo/*, SetOptions.merge()*/);
+            try {
+                // future.get() blocks on response
+                System.out.println("New entry created successfully!\nUpdate time : " + future.get().getUpdateTime());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            galleryInfo.put("gName", "AutoGen Gallery");
+            galleryInfo.put("location", new GeoPoint(0.00, 170.00));
+            // Add a new document (asynchronously) in collection "galleries" with auto-generated id
+            ApiFuture<DocumentReference> docRef = db.collection("galleries").add(galleryInfo);
+            try {
+                // future.get() blocks on response
+                System.out.println("Added document with ID: " + docRef.get().getId());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static void readAndPrintAllArtworks(Firestore db) {
         // asynchronously retrieve all users
         ApiFuture<QuerySnapshot> query = db.collection("artworks").get();
 
